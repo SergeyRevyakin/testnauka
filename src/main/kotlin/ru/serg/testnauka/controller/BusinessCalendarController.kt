1402856e178e91fbaa6d2fd0aba102180a90c7a2
SergeyRevyakin <sarevyakin@gmail.com>
2020-05-22 02:37:03 +0300

    All tables done TODO full API

--- src/main/kotlin/ru/serg/testnauka/controller/BusinessCalendarController.kt
+++ src/main/kotlin/ru/serg/testnauka/controller/BusinessCalendarController.kt
@@ -2,11 +2,13 @@ package ru.serg.testnauka.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.serg.testnauka.dao.BusinessCalendarRepository
 import ru.serg.testnauka.dao.CalendarCodesRepository
 import ru.serg.testnauka.model.BusinessCalendar
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/businesscalendar/")
@@ -19,4 +21,13 @@ class BusinessCalendarController {
 
     @GetMapping("/all")
     fun getFullCalendar(): List<BusinessCalendar> = businessCalendarRepository.findAll()
+
+    @GetMapping("/date={dateString}")
+    fun getCalendarByDay(@PathVariable dateString: String): List<BusinessCalendar>?{
+        val date = LocalDate.parse(dateString)
+//        if (businessCalendarRepository.findByDate(date)?.isNotEmpty()!!){
+//        return businessCalendarRepository.findByDate(date)}
+//        else return null//listOf<BusinessCalendar>()
+        return businessCalendarRepository.findByDate(date)
+    }
 }
--- src/main/kotlin/ru/serg/testnauka/dao/BusinessCalendarRepository.kt
+++ src/main/kotlin/ru/serg/testnauka/dao/BusinessCalendarRepository.kt
@@ -1,9 +1,11 @@
 package ru.serg.testnauka.dao
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import ru.serg.testnauka.model.BusinessCalendar
+import java.time.LocalDate
 
 @Repository
-interface BusinessCalendarRepository:JpaRepository<BusinessCalendar,Int>
+interface BusinessCalendarRepository:JpaRepository<BusinessCalendar,Int>{
+    fun findByDate(date: LocalDate):List<BusinessCalendar>?
+}
